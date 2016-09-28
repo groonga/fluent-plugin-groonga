459b648bd66b13ce7a388edd7323007f660a72dc
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-09-28 14:42:19 +0900

    Really send response on error

--- lib/fluent/plugin/in_groonga.rb
+++ lib/fluent/plugin/in_groonga.rb
@@ -279,9 +279,8 @@ module Fluent
           write("Content-Length: 0\r\n")
           write("\r\n")
           disable
-          @repeater.close
           on_write_complete do
-            close
+            @repeater.close
           end
         end
       end
